b193a2ab6584537c93daa229851f59c1cbe7c31b
Roberto <rvalverdem.85@gmail.com>
2014-07-25 11:47:05 -0600

    propiedades y constructor para cada objeto

--- ColasBancarias.Entidades/OBJETOS/ObjCajero.vb
+++ ColasBancarias.Entidades/OBJETOS/ObjCajero.vb
@@ -1,3 +1,19 @@
 ﻿Public Class ObjCajero
 
+    Property id_cajero As Integer
+    Property cedula_cajero As String
+    Property nombre_cajero As String
+    Property estado_cajero As String
+
+    Sub New()
+
+        id_cajero = Integer.MinValue
+        cedula_cajero = String.Empty
+        nombre_cajero = String.Empty
+        estado_cajero = String.Empty
+
+    End Sub
+
+
+
 End Class
