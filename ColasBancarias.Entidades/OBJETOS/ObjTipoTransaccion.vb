093549cc0f907079797fa01462da49aa18d9386f
Roberto <rvalverdem.85@gmail.com>
2014-08-03 23:04:07 -0600

    TRIGGER

--- ColasBancarias.Entidades/OBJETOS/ObjTipoTransaccion.vb
+++ ColasBancarias.Entidades/OBJETOS/ObjTipoTransaccion.vb
@@ -3,22 +3,21 @@
     Public Class ObjTransaccion
 
 
-        Property id_cliente As Integer
+        Property nombre_cliente As String
         Property id_cajero As Integer
         Property tipo_transaccion As String
         Property hora_transaccion As Date
-        Property nombre_cliente As String
-        Property nombre_cajero As String
+
+
 
 
         Sub New()
 
-            id_cliente = Integer.MinValue
+            nombre_cliente = String.Empty
             id_cajero = Integer.MinValue
             tipo_transaccion = String.Empty
             hora_transaccion = Date.MinValue
-            nombre_cajero = String.Empty
-            nombre_cliente = String.Empty
+
 
         End Sub
 
