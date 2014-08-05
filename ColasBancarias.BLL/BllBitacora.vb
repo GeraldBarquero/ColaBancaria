5421276e0b49d7fa2b336fceb9430b53c483c95d
Roberto <rvalverdem.85@gmail.com>
2014-08-04 21:50:30 -0600

    Grv_Bitacora agregada

---
+++ ColasBancarias.BLL/BllBitacora.vb
@@ -0,0 +1,13 @@
+﻿Imports ColasBancarias.DAO
+Imports ColasBancarias.Entidades
+
+Public Class BllBitacora
+    Dim ejecucion As New MetodosBitacora
+
+    Public Function llenarBitacora()
+
+        Dim tablaListaBitacora = ejecucion.listaBitacora()
+
+        Return tablaListaBitacora
+    End Function
+End Class
